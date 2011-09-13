2c1d91dcb330d0470fa1a24887c1d9c8bcbe18b1
Alistair Buxton <a.j.buxton@gmail.com>
2011-09-13 05:30:02 +0100

    Rework hamming to work better on subpages with many blank chars.

--- subpagesquash.py
+++ subpagesquash.py
@@ -15,11 +15,30 @@ class Page(object):
         ((self.m,self.r),e) = mrag(self.array[0][:2])
         (self.p,e) = page(self.array[0][2:4])
         (self.s,self.c),self.e = subcode_bcd(self.array[0][4:10])
-        self.blanks = ((self.array == ord(' ')).sum(axis=1) > 35).sum()
 
         # remove parity
-        for n in range(2, 26):
-            self.array[n][2:] &= 0x7f
+        self.array[2:,2:] &= 0x7f
+
+        # check if row n-1 is double height
+        self.no_double_on_prev = ((self.array[1:-1,2:]) != 0x0d).all(axis=1)
+        # row 1 can't contain double but might due to not being printable
+        self.no_double_on_prev[0] = True
+
+        # calculate a target threshold for each line
+        # based on the number of non-blank characters
+
+        # first count non-blanks
+        self.threshold = ((self.array[2:,2:] != ord(' ')).sum(axis=1))
+
+        # if non-blanks <= 5, don't require a match (set threshold to 0)
+        # also ignore rows following a double height row
+        self.threshold *= ((self.threshold > 5) & (self.no_double_on_prev))
+
+        # some proportion of non-blanks must match in the rest of the lines
+        self.threshold *= 0.5
+
+        # sum required threshold for each line to get total threshold
+        self.threshold_sum = self.threshold.sum() * 1.5
 
         try:
             self.ds = int("%x" % self.s, 10)
@@ -29,9 +48,13 @@ class Page(object):
         self.goodrows = (rows == Page.rows)
 
     def hamming(self, other):
-        h = (self.array != other.array).sum(axis=1)
-        h *= self.goodrows
-        return (h < 20).all() and h.sum() < 100
+        # compute the similarity/difference between two subpages
+        # if similar enough to squash them into a single subpage, return true
+        # note: no point checking rows 0 and 1. they will always match for 
+        # all subpages.
+
+        h = ((self.array[2:] != ord(' ')) & (self.array[2:] == other.array[2:])).sum(axis=1)
+        return ((h >= self.threshold)).all() and h.sum() >= self.threshold_sum
         #return h.sum() < 200
 
     def to_html(self, anchor):
@@ -43,17 +66,13 @@ class Page(object):
         body.append(line)
 
         i = 2
-        while i < 26:
-            # if previous line contains double height chars, skip this one
+        for i in range(2,26):
+          if self.no_double_on_prev[i-2]:
             p = Printer(self.array[i][2:])
             if i == 25 and self.rows[1] == 27:
                 p.set_fasttext(self.array[1], self.m)
             body.append(p.string_html())
             # skip a line if this packet contained double height chars
-            if ((self.array[i][2:]&0x7f) == 0x0d).any():
-                i += 2
-            else:
-                i += 1
 
         head = '<div class="subpage" id="%d">' % self.s
 
