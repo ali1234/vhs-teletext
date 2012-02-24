31ad8ea43bff737491d39fe8734d06af95f1849c
Alistair Buxton <a.j.buxton@gmail.com>
2012-02-24 19:22:37 +0000

    Add a finder for channel 5

--- finders.py
+++ finders.py
@@ -147,9 +147,13 @@ BBCOld = Finder("          CEEFAX 217  Wed 25 Dec \x0318:29/53",
                 "HHHHHHHHHHeeeeeeemhheeDAYe39eMONee"+"29e59e59", 
                 name="BBC Old Packet 0", row=0)
 
-Central = Finder("          Central  217 Wed 25 Dec 18:29/53",
-                 "HHHHHHHHHHeeeeeeeppmhhpDAYp39pMONp29e59p59", 
+Central = Finder("          Central  217 Wed 25 Dec 18:29:53",
+                 "HHHHHHHHHHeeeeeeeeemhheDAYe39eMONp29e59e59", 
                  name="Central Packet 0", row=0)
+
+FiveText = Finder("          \x06   5 text   \x07255 02 May\x031835:21",
+                  "HHHHHHHHHHe"+"eeeeeeeeeeeee"+"mhhe39eMONe"+"2959e59", 
+                  name="5 Text Packet 0", row=0)
 
 TeletextLtd = Finder("          \x04\x1d\x03Teletext\x07 \x1c100 May05\x0318:29:53",
                      "HHHHHHHHHHe"+"e"+"e"+"eeeeeeeee"+"ee"+"mhheMON39e"+"29e59e59", 
@@ -159,7 +163,7 @@ FourTel = Finder("          4-Tel 307 Sun 26 May\x03C4\x0718:29:53",
                  "HHHHHHHHHHeeeeeemhheDAYe39eMONe"+"eee"+"29e59e59", 
                   name="4Tel Packet 0", row=0)
 
-all_headers = [BBC, BBCOld, Central, TeletextLtd, FourTel]
+all_headers = [BBC, BBCOld, Central, TeletextLtd, FourTel, FiveText]
 
 # there are two types of broadcast packet. one has 8/4 PDC data and the other
 # has no encoding (not even parity). the latter is almost impossible to 
