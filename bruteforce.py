ce01f73b18e35792d291e12a1e0862ac45b593c0
Alistair Buxton <a.j.buxton@gmail.com>
2012-02-26 22:24:13 +0000

    bruteforce.py: Run it on a vbi file to try a range of possible deconvolution parameters.
    ./bruteforce.py data/vbi/00001000.vbi > output
    less -r output
    
    Examine the lines to determine which values give best results. Yes, it is slow.

---
+++ bruteforce.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python
+
+# * Copyright 2011 Alistair Buxton <a.j.buxton@gmail.com>
+# *
+# * License: This program is free software; you can redistribute it and/or
+# * modify it under the terms of the GNU General Public License as published
+# * by the Free Software Foundation; either version 3 of the License, or (at
+# * your option) any later version. This program is distributed in the hope
+# * that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+# * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# * GNU General Public License for more details.
+
+# This is the main data analyser.
+
+import sys
+
+import numpy as np
+from scipy.interpolate import interp1d
+from scipy.ndimage import gaussian_filter1d as gauss
+from scipy.optimize import fminbound
+
+import pylab
+
+from util import paritybytes, setbyte, normalise, hammbytes, allbytes, mrag, notzero
+
+import config
+
+import time
+import printer
+from vbi import Vbi
+
+def process_file(filename):
+  ans = []
+  try:
+    f = file(filename).read()
+    for line in range(32):
+        print line
+        offset = line*2048
+        vbiraw = np.array(np.fromstring(f[offset:offset+2048], dtype=np.uint8), dtype=np.float32)
+        for tl in np.arange(1.05,1.2,0.05):
+         for th in np.arange(2.1,2.4,0.05):
+          for g in np.arange(1.1,5.2,1.0):
+           for go in np.arange(1.1,5.2,1.0):
+            v = Vbi(vbiraw, thresh_low=tl, thresh_high=th, gauss_sd=g, gauss_sd_offset=go)
+            tmp = v.find_offset_and_scale()
+            if tmp:
+                packet = v.deconvolve()
+                print printer.do_print(packet), "thresh_low=%1.2f thresh_high=%1.2f gauss_sd=%1.2f gauss_sd_offset=%1.2f" % (tl, th, g, go)
+            else:
+                packet = None
+        
+        
+  except IOError:
+    pass
+
+  return (filename, ans)  
+
+if __name__ == '__main__':
+
+
+    #do_file('data/0022/vbi/00171104.vbi')
+    process_file(sys.argv[1])
+    #do_file('data/0022/vbi/00171106.vbi')
+    #do_file('data/0022/vbi/00171107.vbi')
+    #do_file('data/0022/vbi/00171108.vbi')
+
+
+
+
+
+
+
