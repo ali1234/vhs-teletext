ee941ddd195f52453f84f20957b7d5fb1849ce97
Alistair Buxton <a.j.buxton@gmail.com>
2016-02-28 22:12:40 +0000

    Add De Bruijn data to repo and pattern data to setup.py

--- setup.py
+++ setup.py
@@ -4,22 +4,13 @@ import os
 
 from distutils.core import setup
 
-from teletext.vbi.training import save_pattern
-
-debruijn = os.path.join('teletext', 'vbi', 'data', 'debruijn.dat')
-try:
-    open(debruijn)
-except:
-    save_pattern(debruijn)
-
-
 setup(name='vhs-teletext',
         version='0.1',
         author='Alistair Buxton',
         author_email='a.j.buxton@gmail.com',
         url='http://github.com/ali1234/vhs-teletext',
         packages=['teletext', 'teletext.vbi', 'teletext.t42', 'teletext.misc'],
-        package_data={'teletext.vbi': ['data/debruijn.dat']},
+        package_data={'teletext.vbi': ['data/debruijn.dat', 'data/parity.dat', 'data/hamming.dat']},
         scripts=['deconvolve', 't42pipe', 'vbiview', 'vbicat', 'training'],
         requires=['numpy', 'scipy'],
     )
