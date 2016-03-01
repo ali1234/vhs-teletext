5dc0236c598dfb01aa9f830c77972b0bd2f194f1
Alistair Buxton <a.j.buxton@gmail.com>
2016-03-01 19:59:48 +0000

    Refactor Printer class into PrinterANSI and PrinterHTML.
    Instead of:
    
        Printer(...).string_ansi()
    
    use:
    
        str(PrinterANSI(...))

--- teletext/t42/printer.py
+++ teletext/t42/printer.py
@@ -2,7 +2,7 @@ import re
 from coding import mrag_decode, page_decode, subcode_bcd_decode, hamming8_decode
 import numpy as np
 
-class Printer(object):
+class PrinterANSI(object):
 
     def __init__(self, tt, codepage=0):
         tt = tt&0x7f
@@ -23,6 +23,7 @@ class Printer(object):
         # anchor for header links so we can bookmark a subpage
         self.anchor = ""
 
+
     def set_fasttext(self, data, mag):
         self.fasttext = True
         self.links = []
@@ -34,6 +35,7 @@ class Printer(object):
             if m == 0:
                 m = 8
             self.links.append("%1d%02x" % (m,p))
+
 
     def ttchar(self, c):
         if self.mosaic and (c < ord('@') or c > ord('_')):
@@ -52,62 +54,38 @@ class Printer(object):
             else:
                 return unichr(c)
 
-    def htmlspanstyle(self, fg=None, bg=None):
-        return '<span class="f%d b%d%s%s%s%s">' % ((fg or self.fg), (bg or self.bg), 
-                      (" dh" if self.double else ""), (" fl" if self.flash else ""),
-                      (" cn" if self.conceal else ""), (" bx" if self.boxed else " nx"))
 
-    def setstyle(self, html, fg=None, bg=None):
-        if html:
-            link = ''
-            linkclose = ''
-            if self.fasttext:
-                if self.flinkopen:
-                    linkclose = '</a>'
-                    self.flinkopen = False
-                if self.fg in [1,2,3,6]:
-                    link = '<a href="%s.html">' % self.links[[1,2,3,6].index(self.fg)]
-                    self.flinkopen = True
-                
-            return linkclose+'</span>'+self.htmlspanstyle()+link
+    def setstyle(self, fg=None, bg=None):
+        return '\033[3%dm\033[4%dm' % ((fg or self.fg), (bg or self.bg))
 
-        else:
-            return '\033[3%dm\033[4%dm' % ((fg or self.fg), (bg or self.bg))
 
-    def linkify(self, html):
-        e = '([^0-9])([0-9]{3})([^0-9]|$)'
-        def repl(match):
-            return '%s<a href="%s.html%s">%s</a>%s' % (match.group(1), match.group(2), self.anchor, match.group(2), match.group(3))
-        p = re.compile(e)
-        return p.sub(repl, html)
-
-    def transform(self, c, html=False):
+    def transform(self, c):
         h = c&0xf0
         l = c&0x0f
         if h == 0x0:
             if l < 8:
                 self.fg = l
                 self.conceal = False
-                ret = ' '+self.setstyle(html)
+                ret = ' '+self.setstyle()
                 self.mosaic = False
             elif l == 0x8: # flashing
                 self.flash = True
-                ret = ' '+self.setstyle(html)
+                ret = ' '+self.setstyle()
             elif l == 0x9: # steady
                 self.flash = False
-                ret = ' '+self.setstyle(html)
+                ret = ' '+self.setstyle()
             elif l == 0xa: # flashing
                 self.boxed = True
-                ret = ' '+self.setstyle(html)
+                ret = ' '+self.setstyle()
             elif l == 0xb: # steady
                 self.boxed = False
-                ret = ' '+self.setstyle(html)
+                ret = ' '+self.setstyle()
             elif l == 0xc: # single height
                 self.double = False
-                ret = ' '+self.setstyle(html)
+                ret = ' '+self.setstyle()
             elif l == 0xd: # double height
                 self.double = True
-                ret = ' '+self.setstyle(html)
+                ret = ' '+self.setstyle()
             else:
                 ret = ' '
                 #print hex(int(c&0xff))
@@ -115,12 +93,12 @@ class Printer(object):
             if l < 8:
                 self.fg = l
                 self.conceal = False
-                ret = ' '+self.setstyle(html)
+                ret = ' '+self.setstyle()
                 self.mosaic = True
                 self.solid = True
             elif l == 0x8: # conceal
                 self.conceal = True
-                ret = ' '+self.setstyle(html)
+                ret = ' '+self.setstyle()
             elif l == 0x9:
                 self.solid = True
                 ret = ' '
@@ -129,10 +107,10 @@ class Printer(object):
                 ret = ' '
             elif l == 0xc:
                 self.bg = 0
-                ret = self.setstyle(html)+' '
+                ret = self.setstyle()+' '
             elif l == 0xd:
                 self.bg = self.fg
-                ret = self.setstyle(html)+' '
+                ret = self.setstyle()+' '
             else:
                 ret = ' '
                 #print hex(int(c&0xff))
@@ -141,9 +119,51 @@ class Printer(object):
 
         return ret
 
-    def string_html(self):
+
+    def __str__(self):
+        head = self.setstyle(fg=7, bg=0)
+        body = "".join([self.transform(x) for x in self.tt])
+        return head+body.encode('utf8')+'\033[0m'
+
+
+
+class PrinterHTML(object):
+
+    def __init__(self, tt, codepage=0):
+        PrinterANSI.__init__(self, tt, codepage)
+
+
+    def htmlspanstyle(self, fg=None, bg=None):
+        return '<span class="f%d b%d%s%s%s%s">' % ((fg or self.fg), (bg or self.bg), 
+                      (" dh" if self.double else ""), (" fl" if self.flash else ""),
+                      (" cn" if self.conceal else ""), (" bx" if self.boxed else " nx"))
+
+
+    def setstyle(self, html, fg=None, bg=None):
+        link = ''
+        linkclose = ''
+        if self.fasttext:
+            if self.flinkopen:
+                linkclose = '</a>'
+                self.flinkopen = False
+            if self.fg in [1,2,3,6]:
+                link = '<a href="%s.html">' % self.links[[1,2,3,6].index(self.fg)]
+                self.flinkopen = True
+                
+        return linkclose+'</span>'+self.htmlspanstyle()+link
+
+
+    def linkify(self, html):
+        e = '([^0-9])([0-9]{3})([^0-9]|$)'
+        def repl(match):
+            return '%s<a href="%s.html%s">%s</a>%s' % (match.group(1), match.group(2), self.anchor, match.group(2), match.group(3))
+        p = re.compile(e)
+        return p.sub(repl, html)
+
+
+    def __str__(self):
         head = self.htmlspanstyle(fg=7, bg=0)
-        body = "".join([self.transform(x, html=True) for x in self.tt])
+        body = "".join([self.transform(x) for x in self.tt])
         foot = '</span>'
         if self.fasttext:
             if self.flinkopen:
@@ -151,12 +171,6 @@ class Printer(object):
         else:
             body = self.linkify(body)
         return head+body.encode('utf8')+foot+'\n'
-
-    def string_ansi(self):
-        head = self.setstyle(html=False, fg=7, bg=0)
-        body = "".join([self.transform(x, html=False) for x in self.tt])
-        return head+body.encode('utf8')+'\033[0m'
-
 
 
 
@@ -171,7 +185,7 @@ def do_print(tt):
         (p,e) = page_decode(tt[2:4])
         ((s,c),e) = subcode_bcd_decode(tt[4:10])
         ret += "   P%1d%02x " % (m,p)
-        ret += Printer(tt[10:]).string_ansi()
+        ret += str(PrinterANSI(tt[10:]))
         ret += " %04x %x" % (s,c)
     elif r == 30: # broadcast service data
         # designation code
@@ -184,7 +198,7 @@ def do_print(tt):
             ret += "(PDC) "
         else:
             ret += "(NET) "
-        ret += Printer(tt[22:]).string_ansi()
+        ret += str(PrinterANSI(tt[22:]).string_ansi())
     elif r == 27: # broadcast service data
         # designation code
         (d,e) = hamming8_decode(tt[2])
@@ -195,7 +209,7 @@ def do_print(tt):
             ((s,m),e) = subcode_bcd_decode(tt[nn+5:nn+9])
             ret += " %1d%02x:%04x " % (m,p,s)
     else:
-        ret += Printer(tt[2:]).string_ansi()
+        ret += str(PrinterANSI(tt[2:]))
 
     return ret
 
