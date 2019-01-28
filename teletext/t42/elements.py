9c94ec4755194018a9fbafdfbdbfde5edf8631bf
ZXGuesser <alistair@zxnet.co.uk>
2019-01-28 17:19:30 +0000

    fix off by one error in elements PageHeader and use control bits in subpage from_packets

--- teletext/t42/elements.py
+++ teletext/t42/elements.py
@@ -49,7 +49,7 @@ class PageHeader(object):
             values.append(v)
 
         subpage = (values[0] & 0x7f) | ((values[1] & 0x3f) <<8)
-        control = (values[0] >> 7) | (values[1] >> 6) | (values[2] << 3)
+        control = (values[0] >> 7) | (values[1] >> 5) | (values[2] << 3)
         return cls(page, subpage, control, errors)
 
     def to_bytes(self):
