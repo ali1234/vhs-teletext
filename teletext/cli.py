a2f9fafc274dd044b0c47eaba8e5bb4010c810d3
Alistair Buxton <a.j.buxton@gmail.com>
2019-02-05 22:03:25 +0000

    Only define arguments/options once

--- teletext/cli.py
+++ teletext/cli.py
@@ -1,6 +1,5 @@
 import importlib
 import sys
-from contextlib import redirect_stdout
 
 import click
 from tqdm import tqdm
@@ -18,17 +17,29 @@ def to_file(packets, f, attr):
         yield p
 
 
+def baseparams(f):
+    for d in [
+         click.argument('input', type=click.File('rb'), default='-'),
+         click.option('--start', type=int, default=0, help='Start at the Nth line of the input file.'),
+         click.option('--stop', type=int, default=None, help='Stop before the Nth line of the input file.'),
+         click.option('--step', type=int, default=1, help='Process every Nth line from the input file.'),
+         click.option('--limit', type=int, default=None,
+                        help='Stop after processing N lines from the input file.'),
+         click.option('--mags', '-m', type=int, multiple=True, default=range(9)),
+         click.option('--rows', '-r', type=int, multiple=True, default=range(32)),
+         click.option('-o', '--output',
+                        type=(click.Choice(['auto', 'text', 'ansi', 'bar', 'bytes']), click.File('wb')),
+                        multiple=True, default=[('auto', '-')]),
+    ]:
+        f = d(f)
+    return f
+
 
 @click.command()
-@click.argument('input', type=click.File('rb'), default='-')
-@click.option('--start', type=int, default=0, help='Start at the Nth line of the input file.')
-@click.option('--stop', type=int, default=None, help='Stop before the Nth line of the input file.')
-@click.option('--step', type=int, default=1, help='Process every Nth line from the input file.')
-@click.option('--limit', type=int, default=None, help='Stop after processing N lines from the input file.')
-@click.option('--mags', '-m', type=int, multiple=True, default=range(9))
-@click.option('--rows', '-r', type=int, multiple=True, default=range(32))
-@click.option('-o', '--output', type=(click.Choice(['auto', 'text', 'ansi', 'bar', 'bytes']), click.File('wb')), multiple=True, default=[('auto', '-')])
+@baseparams
 def pipe(input, start, stop, step, limit, mags, rows, output):
+
+    """Demultiplex and display t42 packet streams."""
 
     chunks = FileChunker(input, 42, start, stop, step, limit)
     bar = tqdm(chunks, unit=' Lines', dynamic_ncols=True)
@@ -42,25 +53,13 @@ def pipe(input, start, stop, step, limit, mags, rows, output):
         pass
 
 
-# TODO: parser.add_argument('-H', '--headers', help='Synonym for --ansi --numbered --rows 0.', action='store_true')
-
 @click.command()
-@click.argument('input', type=click.File('rb'))
-@click.option('--start', type=int, default=0, help='Start at the Nth line of the input file.')
-@click.option('--stop', type=int, default=None, help='Stop before the Nth line of the input file.')
-@click.option('--step', type=int, default=1, help='Process every Nth line from the input file.')
-@click.option('--limit', type=int, default=None, help='Stop after processing N lines from the input file.')
-@click.option('--mags', '-m', type=int, multiple=True, default=range(9))
-@click.option('--rows', '-r', type=int, multiple=True, default=range(32))
-@click.option('-o', '--output', type=(click.Choice(['auto', 'text', 'ansi', 'bar', 'bytes']), click.File('wb')), multiple=True, default=[('auto', '-')])
-#@click.option('-a/-t', '--ansi/--t42', default=sys.stdout.isatty(), help='Force output type.')
-#@click.option('-n', '--numbered', is_flag=True, help='When output is ansi, number the lines according to position in input file.')
+@baseparams
 @click.option('-c', '--config', default='bt8x8_pal', help='Capture card configuration. Default: bt8x8_pal.')
-#@click.option('-T', '--threads', type=int, default=1, help='Number of threads. Default: 1.')
 @click.option('-C', '--force-cpu', is_flag=True, help='Disable CUDA even if it is available.')
 @click.option('-e', '--extra_roll', type=int, default=4, help='')
-#@click.option('-S', '--squash', type=int, default=1, help='Merge N consecutive rows to reduce output.')
 def deconvolve(input, start, stop, step, limit, mags, rows, output, config, force_cpu, extra_roll, ):
+
     """Deconvolve raw VBI samples into Teletext packets."""
 
     from teletext.vbi.line import Line
