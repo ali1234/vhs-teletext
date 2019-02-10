9be90f2cd5fb077bdd670eeea916dbf43035f7ac
Alistair Buxton <a.j.buxton@gmail.com>
2019-02-10 02:04:43 +0000

    Fix-up subpage module
    HTML encoding is still broken for now.

--- teletext/subpage.py
+++ teletext/subpage.py
@@ -1,63 +1,62 @@
-from .packet import *
-from .printer import PrinterHTML
+import numpy as np
 
-from teletext.all import All
+from .packet import Packet
+from .elements import Displayable
+
 
 class Subpage(object):
-    control = ControlBits()
 
-    def __init__(self, fill=0x20, links=[None for n in range(6)]):
-        self.displayable = numpy.full((40, 25), fill, dtype=numpy.uint8)
-        self.control = 0
-        self.__links = [link if link else PageLink() for link in links]
+    def __init__(self):
+        self._array = np.zeros((32, 42), dtype=np.uint8)
+        self._numbers = np.full((32,), -100, dtype=np.int64)
+
+    def row(self, row):
+        return Packet(self._array[row, :])
 
     @property
-    def links(self):
-        return self.__links
+    def header(self):
+        return Packet(self._array[0, :]).header
+
+    @property
+    def fastext(self):
+        return Packet(self._array[27, :]).fastext
+
+    @property
+    def displayable(self):
+        return Displayable(self._array[1:26,2:])
 
     @staticmethod
     def from_packets(packet_iter):
         s = Subpage()
 
         for p in packet_iter:
-            if type(p) == HeaderPacket:
-                s._original_subpage = p.header.subpage
-                s._original_page = p.header.page
-                s._original_magazine = p.mrag.magazine
-                s._original_displayable = p.displayable
-                s.control = p.header.control
-            elif type(p) == FastextPacket:
-                for i in range(6):
-                    s.links[i] = p.links[i]
-            elif type(p) == DisplayPacket:
-                s.displayable[:,p.mrag.row-1] = p.displayable
-
+            s._array[p.mrag.row, :] = p[:]
+            s._numbers[p.mrag.row] = -1 if p.number is None else -2
         return s
 
-    def to_packets(self, magazineno, pageno, subpageno, header_displayable=numpy.full((32,), 0x20, dtype=numpy.uint8)):
-        yield HeaderPacket(Mrag(magazineno, 0), PageHeader(pageno, subpageno, self.control), header_displayable)
-        for i in range(0, 25):
-            if (self.displayable[:,i] != 0x20).any():
-                yield DisplayPacket(Mrag(magazineno, i+1), self.displayable[:,i])
-        yield FastextPacket(Mrag(magazineno, 27), self.links)
+    @property
+    def packets(self):
+        for n, a in enumerate(self._array):
+            if self._numbers[n] > -100:
+                yield Packet(a, number=None if self._numbers[n] < 0 else self._numbers[n])
 
-
-    def to_html(self, magazineno, pageno, subpageno, header_displayable=numpy.full((32,), 0x20, dtype=numpy.uint8), pages_set=All):
-        body = []
-
-        p = PrinterHTML(header_displayable)
-        p.anchor = '#%04X' % subpageno
-        body.append('   <span class="pgnum">P%d%02x</span> ' % (magazineno, pageno) + str(p))
-
-        for i in range(0,25):
-            if i == 0 or numpy.all(self.displayable[:,i-1] != 0x0d):
-                p = PrinterHTML(self.displayable[:,i], pages_set=pages_set)
-                if i == 23:
-                    p.fastext = True
-                    p.links = ['%d%02X' % (l.magazine, l.page) for l in self.links]
-                body.append(str(p))
-
-        head = '<div class="subpage" id="%04X">' % subpageno
-
-        return head + "".join(body) + '</div>'
+# still broken
+#    def to_html(self, magazineno, pageno, subpageno, header_displayable=numpy.full((32,), 0x20, dtype=numpy.uint8), pages_set=All):
+#        body = []
+#
+#        p = PrinterHTML(header_displayable)
+#        p.anchor = '#%04X' % subpageno
+#        body.append('   <span class="pgnum">P%d%02x</span> ' % (magazineno, pageno) + str(p))
+#
+#        for i in range(0,25):
+#            if i == 0 or numpy.all(self.displayable[:,i-1] != 0x0d):
+#                p = PrinterHTML(self.displayable[:,i], pages_set=pages_set)
+#                if i == 23:
+#                    p.fastext = True
+#                    p.links = ['%d%02X' % (l.magazine, l.page) for l in self.links]
+#                body.append(str(p))
+#
+#        head = '<div class="subpage" id="%04X">' % subpageno
+#
+#        return head + "".join(body) + '</div>'
 
