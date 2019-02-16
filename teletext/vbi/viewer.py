362993f43aca58870bd7dfd72f875c16249b200c
Alistair Buxton <a.j.buxton@gmail.com>
2019-02-16 15:30:09 +0000

    Make VBIViewer look nicer

--- teletext/vbi/viewer.py
+++ teletext/vbi/viewer.py
@@ -1,3 +1,4 @@
+import time
 from itertools import islice
 import numpy as np
 
@@ -7,9 +8,11 @@ from OpenGL.GL import *
 
 class VBIViewer(object):
 
-    def __init__(self, lines, config, name = "VBI Viewer", width=1024, height=512, nlines=32, pass_teletext=True, pass_rejects=False, show_grid=False):
+    def __init__(self, lines, config, name = "VBI Viewer", width=800, height=256, nlines=32, tint=True, show_grid=True, show_slices=False):
         self.config = config
         self.show_grid = show_grid
+        self.show_slices = show_slices
+        self.tint = tint
 
         self.nlines = nlines
 
@@ -21,6 +24,7 @@ class VBIViewer(object):
         glutCreateWindow(name)
 
         glutDisplayFunc(self.display)
+        glutReshapeFunc(self.reshape)
 
         glMatrixMode(GL_PROJECTION)
         glOrtho(0, config.line_length, 0, self.nlines, -1, 1)
@@ -29,90 +33,97 @@ class VBIViewer(object):
 
         glDisable(GL_LIGHTING)
         glDisable(GL_DEPTH_TEST)
+        glEnable(GL_BLEND)
+        glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA)
         glEnable(GL_TEXTURE_2D)
         glBindTexture(GL_TEXTURE_2D, glGenTextures(1))
         glPixelStorei(GL_UNPACK_ALIGNMENT,1)
 
         glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST)
         glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST)
-        glTexEnvf(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_DECAL)
-
+        glTexEnvf(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_MODULATE)
 
         glutMainLoop()
 
-    def display(self):
+    def reshape(self, width, height):
+        self.width = width
+        self.height = height
+        glViewport(0, 0, width, height)
 
-        vbi = np.array([x.orig for x in islice(self.lines_src, 0, self.nlines)], dtype=np.uint8).tostring()
-#        time.sleep(0.1)
-        if not sys.stdout.isatty():
-            sys.stdout.write(vbi)
+    def draw_slice(self, slice, r, g, b, a=1.0):
+        glColor4f(r, g, b, a)
+        glBegin(GL_LINES)
+        glVertex2f(slice.start, 0)
+        glVertex2f(slice.start, self.nlines)
+        glVertex2f(slice.stop, 0)
+        glVertex2f(slice.stop, self.nlines)
+        glEnd()
 
-        if len(vbi) != self.config.line_length*self.nlines:
-#            return
+    def draw_h_grid(self, r, g, b, a=1.0):
+        glColor4f(r, g, b, a)
+        glBegin(GL_LINES)
+        for x in range(self.nlines):
+            glVertex2f(0, x)
+            glVertex2f(2048, x)
+        glEnd()
+
+    def draw_bits(self, r, g, b, a=1.0):
+        glColor4f(r, g, b, a)
+        glBegin(GL_LINES)
+        for x in self.config.bits[::8]:
+            glVertex2f(x, 0)
+            glVertex2f(x, self.nlines)
+        glEnd()
+
+    def draw_lines(self):
+        lines = list(islice(self.lines_src, 0, self.nlines))
+
+        if len(lines) != self.nlines:
             exit(0)
 
         glEnable(GL_TEXTURE_2D)
-        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, self.config.line_length, self.nlines, 0, GL_LUMINANCE, GL_UNSIGNED_BYTE, vbi)
+        for n,l in enumerate(lines[::-1]):
+            glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, self.config.line_length, 1, 0, GL_LUMINANCE, GL_UNSIGNED_BYTE, l.orig.tostring())
+            if self.tint:
+                if l.is_teletext:
+                    glColor4f(0.5, 1.0, 0.7, 1.0)
+                else:
+                    glColor4f(1.0, 0.5, 0.5, 1.0)
 
+            glBegin(GL_QUADS)
 
-        glBegin(GL_QUADS)
+            glTexCoord2f(0, 1)
+            glVertex2f(0, n)
 
-        glTexCoord2f(0, 1)
-        glVertex2f(   0, 0)
+            glTexCoord2f(0, 0)
+            glVertex2f(0, (n+1))
 
-        glTexCoord2f(0, 0)
-        glVertex2f(   0, self.nlines)
+            glTexCoord2f(1, 0)
+            glVertex2f(self.config.line_length, (n+1))
 
-        glTexCoord2f(1, 0)
-        glVertex2f(self.config.line_length, self.nlines)
+            glTexCoord2f(1, 1)
+            glVertex2f(self.config.line_length, n)
 
-        glTexCoord2f(1, 1)
-        glVertex2f(self.config.line_length, 0)
-
-        glEnd()
-
-        if 1:
-
-            glDisable(GL_TEXTURE_2D)
-
-            glBegin(GL_LINES)
-
-        if self.show_grid:
-            glColor3f(0, 0, 0)
-
-            if self.nlines < 65:
-              for x in range(self.nlines):
-                glVertex2f(0, x)
-                glVertex2f(2048, x)
-
-        if self.show_grid:
-            glColor3f(1, 0, 0)
-
-            for x in self.config.line_start_range:
-                glVertex2f(x, 0)
-                glVertex2f(x, self.nlines)
-
-            glColor3f(0, 1, 0)
-            for x in self.config.line_start_pre:
-                glVertex2f(x, 0)
-                glVertex2f(x, self.nlines)
-
-            for x in self.config.line_start_post:
-                glVertex2f(x, 0)
-                glVertex2f(x, self.nlines)
-
-
-            glVertex2f(self.config.line_trim, 0)
-            glVertex2f(self.config.line_trim, self.nlines)
-
-        if self.show_grid:
-            glColor3f(0, 0, 1)
-            for x in self.config.bits[::8]:
-                glVertex2f(x, 0)
-                glVertex2f(x, self.nlines)
-
-        if 1:
             glEnd()
+
+        glDisable(GL_TEXTURE_2D)
+
+    def display(self):
+
+        self.draw_lines()
+
+        if self.show_grid:
+
+            if self.height / self.nlines > 3:
+                self.draw_h_grid(0, 0, 0, 0.25)
+
+            if self.width / 42 > 5:
+                self.draw_bits(0, 0, 0, 0.25)
+
+        if self.show_slices:
+            self.draw_slice(self.config.line_start_slice, 1, 0, 0, 0.5)
+            self.draw_slice(self.config.line_start_pre, 0, 1, 0, 0.5)
+            self.draw_slice(self.config.line_start_post, 0, 0, 1, 0.5)
 
         glutSwapBuffers()
         glutPostRedisplay()
