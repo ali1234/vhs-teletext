c5a1595c88961a0caa9e93304d8dbb82a1284844
Alistair Buxton <a.j.buxton@gmail.com>
2019-02-21 21:10:41 +0000

    Write some docs for the multiprocessing system

--- teletext/mp.py
+++ teletext/mp.py
@@ -4,8 +4,11 @@ import time
 
 import multiprocessing as mp
 
-
 def denumerate(quit_event, work_queue, tmp_queue):
+    """
+    Strips sequence numbers from work_queue items and yields the work.
+    If work_queue is empty and quit_event is set, exit.
+    """
     while True:
         try:
             n, item = work_queue.get(timeout=0.1)
@@ -18,12 +21,16 @@ def denumerate(quit_event, work_queue, tmp_queue):
 
 
 def renumerate(iterator, done_queue, tmp_queue):
+    """
+    Recombines results with the sequence numbers stored in tmp_queue.
+    """
     for item in iterator:
         n = tmp_queue.get()
         done_queue.put((n, item))
 
 
 def slave(function, quit_event, work_queue, done_queue, args, kwargs):
+    """The main function for subprocesses. """
     tmp_queue = mp.Queue() # holds work item numbers to be recombined with the result
     try:
         renumerate(function(denumerate(quit_event, work_queue, tmp_queue), *args, **kwargs), done_queue, tmp_queue)
@@ -32,6 +39,17 @@ def slave(function, quit_event, work_queue, done_queue, args, kwargs):
 
 
 def itermap(function, iterator, processes=1, *args, **kwargs):
+    """
+    Implements a multiprocessing pool similar in function to multiprocessing.Pool.
+    However, Pool.map(f, i) calls f on every item in i individually. f is expected
+    to return the result. itermap(f, i) calls f exactly once for each process it
+    starts, and then delivers an iterator containing work items. f is expected to
+    yield results. In practice, this means you can pass large objects to f and they
+    will only be pickled once rather than for every item in i. It also allows you
+    to do one-time setup at the beginning of f.
+
+    itermap() preserves the ordering of items in the input iterator.
+    """
     if processes == 1:
         yield from function(iterator, *args, **kwargs)
     else:
