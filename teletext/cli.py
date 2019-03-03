9225b155358ca5481a6ca40dda3800400cb26eda
Alistair Buxton <a.j.buxton@gmail.com>
2019-03-03 03:35:11 +0000

    Enable ANSI colour on Windows 10
    You can also install teletext2.tff and set it as the console font
    by clicking the command prompt window icon and selecting properties.

--- teletext/cli.py
+++ teletext/cli.py
@@ -1,5 +1,6 @@
 import multiprocessing
 import os
+import platform
 import stat
 import sys
 from functools import wraps
@@ -16,6 +17,13 @@ from .subpage import Subpage
 from . import pipeline
 
 from .vbi.config import Config
+
+
+if os.name == 'nt' and platform.release() == '10' and platform.version() >= '10.0.14393':
+    # Fix ANSI color in Windows 10 version 10.0.14393 (Windows Anniversary Update)
+    import ctypes
+    kernel32 = ctypes.windll.kernel32
+    kernel32.SetConsoleMode(kernel32.GetStdHandle(-11), 7)
 
 
 def filterparams(f):
