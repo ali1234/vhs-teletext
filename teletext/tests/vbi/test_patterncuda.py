01f69ef1adad8af494fb41aeb4b38b75963af773
Alistair Buxton <a.j.buxton@gmail.com>
2019-03-03 01:15:41 +0000

    Don't try to test CUDA if pycuda is not installed

--- teletext/tests/vbi/test_patterncuda.py
+++ teletext/tests/vbi/test_patterncuda.py
@@ -5,19 +5,25 @@ import unittest
 import numpy as np
 
 from teletext.vbi.pattern import Pattern
-from teletext.vbi.patterncuda import PatternCUDA
+try:
+    from teletext.vbi.patterncuda import PatternCUDA
 
 
-class PatternCUDATestCase(unittest.TestCase):
+    class PatternCUDATestCase(unittest.TestCase):
 
-    def setUp(self):
-        p = os.path.dirname(__file__) + '/../../vbi/data/parity.dat'
-        self.pattern = Pattern(p)
-        self.patterncuda = PatternCUDA(p)
+        def setUp(self):
+            p = os.path.dirname(__file__) + '/../../vbi/data/parity.dat'
+            self.pattern = Pattern(p)
+            self.patterncuda = PatternCUDA(p)
 
-    def test_equal_to_cpu(self):
-        arr = np.arange(256, dtype=np.uint8)
-        a = self.pattern.match(arr)
-        b = self.patterncuda.match(arr)
+        def test_equal_to_cpu(self):
+            arr = np.arange(256, dtype=np.uint8)
+            a = self.pattern.match(arr)
+            b = self.patterncuda.match(arr)
 
-        self.assertTrue(all(a==b), 'CPU and CUDA pattern matching produced different results.')
+            self.assertTrue(all(a==b), 'CPU and CUDA pattern matching produced different results.')
+
+except ModuleNotFoundError as e:
+    if e.args[1] != 'pycuda':
+        raise
+
