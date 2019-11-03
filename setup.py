95a492760ab734842f395744307cc7aa33da4c72
Alistair Buxton <a.j.buxton@gmail.com>
2019-11-03 07:30:29 +0000

    Reformat setup.py

--- setup.py
+++ setup.py
@@ -7,13 +7,23 @@ setup(
     author_email='a.j.buxton@gmail.com',
     url='http://github.com/ali1234/vhs-teletext',
     packages=['teletext', 'teletext.vbi'],
-    package_data={'teletext.vbi': ['data/debruijn.dat', 'data/parity.dat', 'data/hamming.dat', 'data/full.dat']},
+    package_data={
+        'teletext.vbi': [
+            'data/debruijn.dat',
+            'data/parity.dat',
+            'data/hamming.dat',
+            'data/full.dat'
+        ]
+    },
     entry_points={
         'console_scripts': [
             'teletext = teletext.cli:teletext',
         ]
     },
-    install_requires=['numpy', 'scipy', 'click', 'tqdm', 'windows-curses;platform_system=="Windows"'],
+    install_requires=[
+        'numpy', 'scipy', 'click', 'tqdm',
+        'windows-curses;platform_system=="Windows"'
+    ],
     extras_require={
         'spellcheck': ['pyenchant'],
         'CUDA': ['pycuda', 'scikit-cuda'],
