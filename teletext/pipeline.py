5594cf12a703dcadd357f04e3c2737346360c022
Alistair Buxton <a.j.buxton@gmail.com>
2019-11-04 17:14:59 +0000

    Don't mode the fixed parts of packets
    Since we mode together packets on the same row, the mrag will always
    match. Same for dc on packets 26, 27, 28. We can just copy the first
    2 or 3 bytes of any packet instead.

--- teletext/pipeline.py
+++ teletext/pipeline.py
@@ -49,24 +49,24 @@ def subpage_squash(packet_lists, min_duplicates=3, ignore_empty=False):
                 if row in [26, 27, 28]:
                     for dc in range(16):
                         if s.number(row, dc) > -100:
-                            packets = np.stack([sp.packet(row, dc)[:] for sp in splist if sp.number(row, dc) > -100])
+                            packets = [sp.packet(row, dc) for sp in splist if sp.number(row, dc) > -100]
+                            arr = np.stack([p[3:] for p in packets])
+                            s.packet(row, dc)[:3] = packets[0][:3]
                             if row == 27:
-                                s.packet(row, dc)[:] = mode(packets, axis=0)[0][0].astype(np.uint8)
+                                s.packet(row, dc)[3:] = mode(arr, axis=0)[0][0].astype(np.uint8)
                             else:
-                                # Note: The first "triplet" is actually the mrag and dc. These are
-                                # guaranteed to be the same on every packet, so it is fine to squash
-                                # them this way.
-                                t = packets.astype(np.uint32)
+                                t = arr.astype(np.uint32)
                                 t = t[:, 0::3] | (t[:, 1::3] << 8) | (t[:, 2::3] << 16)
                                 result = mode(t, axis=0)[0][0].astype(np.uint32)
-                                s.packet(row, dc)[0::3] = result & 0xff
-                                s.packet(row, dc)[1::3] = (result >> 8) & 0xff
-                                s.packet(row, dc)[2::3] = (result >> 16) & 0xff
+                                s.packet(row, dc)[3::3] = result & 0xff
+                                s.packet(row, dc)[4::3] = (result >> 8) & 0xff
+                                s.packet(row, dc)[5::3] = (result >> 16) & 0xff
                 else:
                     if s.number(row) > -100:
-                        packets = np.stack([sp.packet(row)[:] for sp in splist if sp.number(row) > -100])
-                        r = mode(packets, axis=0)[0][0].astype(np.uint8)
-                        s.packet(row)[:] = r
+                        packets = [sp.packet(row) for sp in splist if sp.number(row) > -100]
+                        arr = np.stack([p[2:] for p in packets])
+                        s.packet(row)[:2] = packets[0][:2]
+                        s.packet(row)[2:] = mode(arr, axis=0)[0][0].astype(np.uint8)
 
             yield s
 
