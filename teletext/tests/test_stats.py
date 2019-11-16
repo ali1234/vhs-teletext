a863234cf4188d7361cc03bcca9d6c10691f4c10
Alistair Buxton <a.j.buxton@gmail.com>
2019-11-16 19:55:47 +0000

    Add test for StatsList

---
+++ teletext/tests/test_stats.py
@@ -0,0 +1,12 @@
+import unittest
+
+
+from teletext.stats import *
+
+class TestStatsList(unittest.TestCase):
+
+    def test_str(self):
+        l = StatsList()
+        l.append('a')
+        l.append('b')
+        self.assertEqual(str(l), 'ab')
