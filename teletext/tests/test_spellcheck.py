2496e7ee1ff513c2c3e589db9ee03678fe205161
Alistair Buxton <a.j.buxton@gmail.com>
2019-11-16 20:48:02 +0000

    Add some tests for the spelling checker

---
+++ teletext/tests/test_spellcheck.py
@@ -0,0 +1,21 @@
+import unittest
+
+from teletext.spellcheck import *
+from teletext.elements import Displayable
+
+
+class TestSpellCheck(unittest.TestCase):
+
+    def setUp(self):
+        self.sc = SpellChecker(language='en_GB')
+
+    def test_case_match(self):
+        src = 'AaAaA'
+        word = 'bbbbb'
+        self.assertEqual(self.sc.case_match(word, src), 'BbBbB')
+
+    def test_suggest(self):
+        # correctly spelled word should be unchanged
+        self.assertEqual(self.sc.suggest('hello'), 'hello')
+        # incorrectly spelled word with known substitutions should be fixed
+        self.assertEqual(self.sc.suggest('dello'), 'hello')
