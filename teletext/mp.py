494689987d8ab599e715e7b3684059f9e3bd6d8c
Alistair Buxton <a.j.buxton@gmail.com>
2019-11-20 01:51:48 +0000

    More refactoring of MP code
    There is now a lot of unit tests which should catch most possible
    ways you can deadlock the MP code. It is also no longer necessary
    to catch ctrl-c/SIGINT, as the main process does not need to keep
    track of items in/out the queue in order to shut down cleanly.

--- teletext/mp.py
+++ teletext/mp.py
@@ -4,8 +4,6 @@ import signal
 
 import multiprocessing as mp
 
-from .sigint import SigIntDefer
-
 
 def denumerate(quit_event, work_queue, tmp_queue):
     """
@@ -45,28 +43,7 @@ def worker(function, started_event, stopped_event, quit_event, work_queue, done_
         stopped_event.set()
 
 
-class PureGeneratorPool(object):
-
-    """
-    Implements a parallel processing pool similar to multiprocessing.Pool. However,
-    Pool.map(f, i) calls f on every item in i individually. f is expected to return
-    the result. PureGeneratorPool.apply(f, i) calls f exactly once for each process
-    it starts, and then delivers an iterator containing work items. f is expected
-    to yield results. In practice, this means you can pass large objects to f and
-    they will only be pickled once rather than for every item in i. It also allows
-    you to do one-time setup at the beginning of f.
-
-    f must be a "pure generator". This means it must yield exactly one result for
-    each item in the iterator, and that result must only depend on the current
-    item being processed. It must not have any mutable state. For example, any
-    function of the form:
-
-        itertools.partial(map, f)
-
-    is a pure generator.
-
-    apply() preserves the ordering of items in the input iterator.
-    """
+class _PureGeneratorPoolMP(object):
 
     def __init__(self, function, processes=1, *args, **kwargs):
         self._processes = processes
@@ -74,10 +51,6 @@ class PureGeneratorPool(object):
         self._args = args
         self._kwargs = kwargs
         self._pool = []
-
-        if self._processes <= 1:
-            # Don't need to set up processes, queues, events.
-            return
 
         ctx = mp.get_context('spawn')
 
@@ -97,14 +70,6 @@ class PureGeneratorPool(object):
             ))
             self._pool.append((p, started_event, stopped_event))
 
-    @property
-    def started(self):
-        return all(p[1].is_set() for p in self._pool)
-
-    @property
-    def stopped(self):
-        return any(p[2].is_set() for p in self._pool)
-
     def __enter__(self):
         for p in self._pool:
             p[0].start()
@@ -114,45 +79,121 @@ class PureGeneratorPool(object):
         return self
 
     def apply(self, iterable):
-        if self._pool:
-            iterable = enumerate(iterable)
+        iterable = enumerate(iterable)
 
-            with SigIntDefer() as sigint:
+        sent_count = 0
+        received_count = 0
 
-                sent_count = 0
-                received_count = 0
+        # Prime the queue with some items.
+        for item in itertools.islice(iterable, 32):
+            self._work_queue.put(item)
+            sent_count += 1
 
-                # Prime the queue with some items.
-                for item in itertools.islice(iterable, 100):
-                    self._work_queue.put(item)
+        # Dict to use for sorting received items back into
+        # their original order.
+        received = {}
+
+        while received_count < sent_count:
+            try:
+                n, item = self._done_queue.get(timeout=0.1)
+            except queue.Empty:
+                if any(p[2].is_set() for p in self._pool):
+                    raise ChildProcessError('A worker process stopped unexpectedly.')
+            else:
+                received[n] = item
+                while received_count in received:
+                    yield received[received_count]
+                    del received[received_count]
+                    received_count += 1
+                try:
+                    self._work_queue.put(next(iterable))
                     sent_count += 1
-
-                # Dict to use for sorting received items back into
-                # their original order.
-                received = {}
-
-                while received_count < sent_count:
-                    n, item = self._done_queue.get()
-                    received[n] = item
-                    while received_count in received:
-                        yield received[received_count]
-                        del received[received_count]
-                        received_count += 1
-                    if sigint.fired:
-                        self._quit_event.set()
-                    else:
-                        try:
-                            self._work_queue.put(next(iterable))
-                            sent_count += 1
-                        except StopIteration:
-                            self._quit_event.set()
-        else:
-            # Single process implementation.
-            yield from self._function(iterable, *self._args, **self._kwargs)
+                except StopIteration:
+                    pass
 
     def __exit__(self, *args):
+        self._quit_event.set()
+        while not all(p[2].is_set() for p in self._pool):
+            try:
+                self._done_queue.get(timeout=0.1)
+            except queue.Empty:
+                pass
         for p in self._pool:
             p[0].join()
+
+
+class _PureGeneratorPoolSingle(object):
+
+    """
+    An implementation of PureGeneratorPool that doesn't use multiple processes.
+    """
+
+    def __init__(self, function, *args, **kwargs):
+        self._function = function
+        self._args = args
+        self._kwargs = kwargs
+        self._work_queue = queue.Queue()
+        self._proc = self._function(self._work, *args, **kwargs)
+
+    @property
+    def _work(self):
+        while True:
+            try:
+                yield self._work_queue.get(block=False)
+            except queue.Empty:
+                return
+
+    def __enter__(self):
+        return self
+
+    def apply(self, iterable):
+        for item in iterable:
+            self._work_queue.put(item)
+            yield next(self._proc)
+
+    def __exit__(self, *args):
+        try:
+            next(self._proc)
+        except StopIteration:
+            pass
+
+
+def PureGeneratorPool(function, processes, *args, **kwargs):
+
+    """
+    Implements a parallel processing pool similar to multiprocessing.Pool. However,
+    Pool.map(f, i) calls f on every item in i individually. f is expected to return
+    the result. PureGeneratorPool.apply(f, i) calls f exactly once for each process
+    it starts, and then delivers an iterator containing work items. f is expected
+    to yield results. In practice, this means you can pass large objects to f and
+    they will only be pickled once rather than for every item in i. It also allows
+    you to do one-time setup at the beginning of f.
+
+    f must be a "pure generator". This means it must yield exactly one result for
+    each item in the iterator, and that result must only depend on the current
+    item being processed. It must not have any mutable state which affects the
+    output. For example, any function of the form:
+
+        itertools.partial(map, f)
+
+    is a pure generator if f is pure.
+
+    And further:
+
+        def gen(g, f, it):
+            g()
+            yield from f(it)
+
+    is a pure generator if f is a pure generator, regardless of whether or not g
+    is pure.
+
+    apply() preserves the ordering of items in the input iterator.
+    """
+
+    if processes > 1:
+        return _PureGeneratorPoolMP(function, processes, *args, **kwargs)
+    else:
+        return _PureGeneratorPoolSingle(function, *args, **kwargs)
 
 
 def itermap(function, iterable, processes=1, *args, **kwargs):
--- teletext/tests/test_mp.py
+++ teletext/tests/test_mp.py
@@ -3,29 +3,82 @@ import unittest
 from teletext.mp import itermap, PureGeneratorPool
 
 
-def func(it):
+def square(it):
     for x in it:
         yield x*x
 
 
-class TestMP(unittest.TestCase):
+callcounter = 0
+def callcount(it):
+    global callcounter
+    callcounter += 1
+    for x in it:
+        yield callcounter
+
+
+def crashy(it):
+    for x in it:
+        if x:
+            raise ValueError('Crashed on purpose.')
+        else:
+            yield x
+
+
+def crashy_quiet(it):
+    import sys
+    sys.stderr = open('/dev/null') # so as not to spam all over the tests
+    crashy(it)
+
+
+
+class TestMPSingle(unittest.TestCase):
+    procs = 1
 
     def setUp(self):
-        self.input = list(range(512))
-        self.result = list(func(self.input))
+        self.input = list(range(200))
+        self.result = list(square(self.input))
+        global callcounter
+        callcounter = 0
 
     def test_single(self):
-        result = list(itermap(func, self.input, processes=1))
+        result = list(itermap(square, self.input, processes=self.procs))
         self.assertListEqual(result, self.result)
 
-    #@unittest.skip # breaks coverage - https://github.com/nedbat/coveragepy/issues/745
-    def test_multi(self):
-        result = list(itermap(func, self.input, processes=2))
-        self.assertListEqual(result, self.result)
+    def test_called_once_single(self):
+        result = list(itermap(callcount, [None] * (self.procs + 1), processes=self.procs))
+        self.assertListEqual(result, [1] * (self.procs + 1))
 
     def test_reuse(self):
-        with PureGeneratorPool(func, processes=2) as pool:
+        with PureGeneratorPool(square, processes=self.procs) as pool:
             result = list(pool.apply(self.input[:100]))
             self.assertListEqual(result, self.result[:100])
             result = list(pool.apply(self.input[100:]))
             self.assertListEqual(result, self.result[100:])
+
+    def test_called_once_reuse(self):
+        with PureGeneratorPool(callcount, processes=self.procs) as pool:
+            for n in range(self.procs + 1):
+                result = list(pool.apply([None]))
+                self.assertListEqual(result, [1])
+
+    def _crashing_iter(self, n):
+        with self.assertRaises(ValueError):
+            list(itermap(crashy, ([False]*n) + [True], processes=self.procs))
+
+    def test_crashing_iter(self):
+        self._crashing_iter(0)
+        self._crashing_iter(1)
+        self._crashing_iter(self.procs + 1)
+        self._crashing_iter(105)
+
+
+class TestMPMulti(TestMPSingle):
+    procs = 2
+
+    def _crashing_iter(self, n):
+        with self.assertRaises(ChildProcessError):
+            list(itermap(crashy_quiet, ([False]*n) + [True], processes=self.procs))
+
+
+class TestMPMulti8(TestMPMulti):
+    procs = 8
