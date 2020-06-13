ccee6c6bb679a704ea1ab93a2322f9475e35ce6f
Alistair Buxton <a.j.buxton@gmail.com>
2020-06-13 14:46:46 +0100

    Actually bump the version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='teletext',
-    version='3.0.0',
+    version='3.1.99',
     author='Alistair Buxton',
     author_email='a.j.buxton@gmail.com',
     url='http://github.com/ali1234/vhs-teletext',
