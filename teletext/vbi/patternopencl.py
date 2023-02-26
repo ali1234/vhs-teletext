39c4f9c52d9ee397727aa1d2de35ed9db1445fbe
Dr. David Alan Gilbert <dave@treblig.org>
2023-02-26 20:04:38 +0000

    VBI/opencl: Add setup and call kernel
    Translation from the CUDA code.
    
    Not got an equivalent of the min pass yet
    
    Signed-off-by: Dr. David Alan Gilbert <dave@treblig.org>

--- teletext/vbi/patternopencl.py
+++ teletext/vbi/patternopencl.py
@@ -41,8 +41,28 @@ class PatternOpenCL(Pattern):
 
         self.queue = cl.CommandQueue(openclctx)
 
-        raise Exception("Not yet implemented")
+        mf = cl.mem_flags
+        # patterns is already float32 (see Pattern __init__)
+        self.patterns_gpu = cl.Buffer(openclctx, mf.READ_ONLY | mf.COPY_HOST_PTR, hostbuf=self.patterns)
+
+        # the input of the correlate -
+        # size copying CUDA code, something like 40chars, 8 bits each, float??
+        self.input_match = cl.Buffer(openclctx, mf.READ_WRITE, 4*((40*8)+16))
+
+        # output of the correlate
+        self.result_match = cl.Buffer(openclctx, mf.READ_WRITE, 4*40*self.n)
 
     def match(self, inp):
-        raise Exception("Not yet implemented")
+        l = (len(inp)//8)-2
+        x = l & -l # highest power of two which divides l, up to 8
+        y = min(1024//x, self.n)
+
+        # copy data in
+        cl.enqueue_copy(self.queue, self.input_match, inp.astype(np.float32))
+        # call corellate
+        self.prg.correlate(self.queue, (l, self.n), None,
+                           self.input_match, self.patterns_gpu, self.result_match,
+                           np.int32(self.start), np.int32(self.end))
+        # TODO do a min pass to find smallest (start with numpy?)
+        #raise Exception("Not yet implemented (match)")
 
