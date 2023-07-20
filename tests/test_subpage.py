60f8d2e65a58ab79361bb567b99e5a5addeb589d
Alistair Buxton <a.j.buxton@gmail.com>
2023-07-21 00:28:33 +0100

    Tests: Reverse arguments so failure output makes sense

--- tests/test_subpage.py
+++ tests/test_subpage.py
@@ -9,7 +9,7 @@ class SubpageTestCase(unittest.TestCase):
 
     def test_checksum(self):
         p = Subpage()
-        self.assertEqual(p.checksum, 0xe23d)
+        self.assertEqual(0xe23d, p.checksum)
         p = Subpage(prefill=True)
-        self.assertEqual(p.checksum, 0xe23d)
+        self.assertEqual(0xe23d, p.checksum)
 
